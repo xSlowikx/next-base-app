85f2ea90b9c19543f2f12a1c1bd03339d1f577f2
Matias Ariel Coco <cocomatias6@gmail.com>
2025-04-14 20:42:00 -0300

    hidden buttons of Google Map

--- src/app/components/google-maps/MapWraper.tsx
+++ src/app/components/google-maps/MapWraper.tsx
@@ -1,18 +1,35 @@
-import { APIProvider, Map, MapCameraChangedEvent } from '@vis.gl/react-google-maps'
-import { MarkersProps } from '@/types/google-maps/types'
-import { Markers } from './Markers'
+import {
+  APIProvider,
+  Map,
+  MapCameraChangedEvent,
+} from "@vis.gl/react-google-maps";
+import { MarkersProps } from "@/types/google-maps/types";
+import { Markers } from "./Markers";
 
-export default function MapWraper (props: { locations: MarkersProps[] }) {
-    return (
-        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
-            <Map
-                mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID || ''}
-                defaultCenter={{ lat: -34.67067374713742, lng: -58.56278911047002 }}
-                defaultZoom={13}
-                onCameraChanged={(ev: MapCameraChangedEvent) => console.log("camera changed:", ev.detail.center, "zoom:", ev.detail.zoom)}
-            >
-                <Markers markers={props.locations} />
-            </Map>
-        </APIProvider>
-    )
+export default function MapWraper(props: { locations: MarkersProps[] }) {
+  return (
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}>
+      <Map
+        mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID || ""}
+        defaultCenter={{ lat: -34.67067374713742, lng: -58.56278911047002 }}
+        defaultZoom={13}
+        onCameraChanged={(ev: MapCameraChangedEvent) =>
+          console.log(
+            "camera changed:",
+            ev.detail.center,
+            "zoom:",
+            ev.detail.zoom
+          )
+        }
+        fullscreenControl={false}
+        mapTypeControl={false}
+        streetViewControl={false}
+        zoomControl={false}
+        clickableIcons={false}
+        disableDefaultUI={true} // Esto apaga todos los controles por defecto
+      >
+        <Markers markers={props.locations} />
+      </Map>
+    </APIProvider>
+  );
 }
