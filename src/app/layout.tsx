5046c24c775b74a98159994273475258f9375c4a
Matias Ariel Coco <cocomatias6@gmail.com>
2025-04-14 20:20:51 -0300

    add dialog and docensa icon in Google Map

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen flex flex-col">
         <ThemeProvider
           attribute="class"
