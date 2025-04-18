7e542649f12b9e7c35f1c5e39eef6d60507a149e
Matias Ariel Coco <cocomatias6@gmail.com>
2025-04-17 23:51:10 -0300

    Refactors SearchBar form styling
    Updates the SearchBar form styling to use consistent padding across different screen sizes, improving the overall layout and visual consistency.

--- src/app/components/home/SearchBar.tsx
+++ src/app/components/home/SearchBar.tsx
@@ -5,7 +5,7 @@ import SearchInput from "./SearchInput";
 export default function SearchBar() {
   return (
     <div className="w-[90%] flex items-center justify-center p-4 mx-auto max-w-xl lg:max-w-2xl">
-      <form className="flex flex-col items-center bg-[#e6dfcc] rounded-xl w-full  md:p-4 md:flex-row md:gap-2 lg:p-6">
+      <form className="flex flex-col items-center bg-[#e6dfcc] rounded-xl w-full p-4 md:flex-row md:gap-2 lg:px-6">
         <CategoryFilter />
 
         <PlaceFilter />
